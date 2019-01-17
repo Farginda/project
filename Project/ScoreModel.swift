9e71e50c642f732413be09271324ef500adbfeea
Farginda <Farginda@wcw-stud-145-109-23-56.wireless.uva.nl>
2019-01-17 11:42:12 +0100

    update

---
+++ Project/ScoreModel.swift
@@ -0,0 +1,19 @@
+//
+//  ScoreModel.swift
+//  Project
+//
+//  Created by Farginda on 1/17/19.
+//  Copyright © 2019 Farginda. All rights reserved.
+//
+
+import Foundation
+
+struct Score: Codable {
+    var name: String
+    var score: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case score
+    }
+}
